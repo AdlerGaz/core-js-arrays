465a2d9e4f123c42bc4170c5609d19c3f1241f55
Yury Maniakhin <manah9191@mail.ru>
2024-12-25 18:32:26 +0300

    feat: complete 'getIdentityMatrix' function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -427,8 +427,16 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  let matrix = Array(n).fill();
+
+  matrix = matrix.map((el, i) => {
+    const arr = Array(n).fill(0);
+    arr[i] = 1;
+    return arr;
+  });
+
+  return matrix;
 }
 
 /**
